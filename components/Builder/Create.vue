6bd0b036a2ad22ecc5734e943017ec867f5a61fb
ZeshanQureshi <z22qures@gmail.com>
2022-09-17 19:19:34 -0400

    Initialized pdf File

--- components/Builder/Create.vue
+++ components/Builder/Create.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <div class="grid grid-cols-6 w-full px-12 mt-16">
     <div class="col-span-2 flex flex-col">
       <h3 class="text-2xl">Tell us about yourself</h3>
@@ -43,6 +44,31 @@
     </div>
     <div class="col-span-4"></div>
   </div>
+  <div class="rounded border-black border-2 float-right position-absolute">
+     <vue-html2pdf
+        :show-layout="false"
+        :float-layout="false"
+        :enable-download="true"
+        :preview-modal="true"
+        :paginate-elements-by-height="1400"
+        filename="hee hee"
+        :pdf-quality="2"
+        :manual-pagination="false"
+        pdf-format="a4"
+        pdf-orientation="landscape"
+        pdf-content-width="800px"
+ 
+        @progress="onProgress($event)"
+        @hasStartedGeneration="hasStartedGeneration()"
+        @hasGenerated="hasGenerated($event)"
+        ref="html2Pdf"
+    >
+        <section slot="pdf-content">
+            <p>Hi</p>
+        </section>
+    </vue-html2pdf>
+    </div>
+   </div>
 </template>
 
 <script>
